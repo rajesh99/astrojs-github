c7d03bec01b717dac7f0c536ae7fb277b19f90e4
boratechlife <boratechlife@gmail.com>
2022-05-01 01:26:58 -0700

    remove vue dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,6 @@
 import { defineConfig } from 'astro/config';
-import installedIntegration from '@astrojs/vue';
 // https://astro.build/config
 export default defineConfig({
-
-    integrations: [
-        // 1. Imported from an installed npm package
-        installedIntegration(), 
-    ],
     outDir: './directory',
     build: {
         // Example: Generate `page.html` instead of `page/index.html` during build.
