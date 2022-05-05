22d038be925f8f5f19f9c694adca477a0e985f21
boratechlife <boratechlife@gmail.com>
2022-05-05 08:46:54 +0300

    add pagination and google places api

---
+++ config.js
@@ -0,0 +1,3 @@
+export default {
+        api_key : "AIzaSyDj7iB7tGksVSd2xhJVqYiaasQO0M3Qn3Y" 
+}
--- src/data/Cities.js
+++ src/data/Cities.js
@@ -2,63 +2,7 @@ export default [
     {
         name:"New York",
     },
-    {
-        name:"Los Angeles",
-    },
-    {
-        name:"Chicago",
-    }, 
-       {
-        name:"Houston",
-    },
-    {
-        name:"Phoenix",
-    },
-    {
-        name:"Philadelphia",
-    },
-    {
-        name:"San Antonio",
-    },
-    {
-        name:"San Diego",
-    },
-    {
-        name:"Dallas",
-    },
-    {
-        name:"San Jose",
-    },
-    {
-        name:"Austin",
-    },
-    {
-        name:"Jacksonville",
-    },
-    {
-        name:"Fort Worth",
-    },
-    {
-        name:"Columbus",
-    },
-    {
-        name:"Indianapolis",
-    },
-    {
-        name:"Charlotte",
-    },
-    {
-        name:"San Francisco",
-    },
-    {
-        name:"Seattle",
-    },
-    {
-        name:"Denver",
-    },
-    {
-        name:"Seattle",
-    },
-    
+ 
+      
 
 ]
