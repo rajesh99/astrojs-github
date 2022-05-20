680f029c9bf431607858acce99c512e5408a0bc5
boratechlife <boratechlife@gmail.com>
2022-05-20 19:05:24 +0300

    fix the directory structure

--- v1-bootstrap/astro.config.mjs
+++ v1-bootstrap/astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     outDir: './dist/directory',
+    base:"./",
     build: {
         // Example: Generate `page.html` instead of `page/index.html` during build.
         format: 'file',
